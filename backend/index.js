ba55989655bdf7f3be7c44b51fb29a34b6b09444
VinothanaBalakrishnan05 <vinothanakrish05@gmail.com>
2025-08-01 22:52:01 +0530

    admin landing page

--- backend/index.js
+++ backend/index.js
@@ -3,14 +3,14 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const userRoutes = require("./routes/userRoutes");
 const adminRoutes = require("./routes/adminRoutes");
-
+require("dotenv").config();
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
 app.use("/user", userRoutes);
-app.use("/admin",adminRoutes);
+app.use("/api",adminRoutes);
 
 app.listen(5000, () => console.log("Server running on port 5000"));
 
